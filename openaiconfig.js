3f8f124c526b7a4ad8b85033e45e710291789434
mwezn <m.w.g.nelson@gmail.com>
2023-01-22 11:18:44 +0000

    sorted out fetch error

--- openaiconfig.js
+++ openaiconfig.js
@@ -8,20 +8,22 @@ const configuration = new openai.Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai2 = new openai.OpenAIApi(configuration);
-async function genImg(){
-    const response = await openai2.createImage({
-        prompt: "a white siamese cat",
+async function genImg(queryTxt){
+  let image_url;
+    await openai2.createImage({
+        prompt: `${queryTxt}`,
         n: 1,
         size: "1024x1024",
       })
       .then(d=>{
-          console.log(d);
-          let image_url=d.data.data[0].url;
+          //console.log(d);
+          image_url=d.data.data[0].url;
           console.log(image_url)
           return image_url;
-
-      })
+        })
       .catch(err=>console.log(err))
+
+      return image_url
 }
 
 module.exports= {genImg};
--- server.js
+++ server.js
@@ -3,15 +3,32 @@ require('dotenv').config();
 const express=require('express');
 const app=express();
 const {genImg}=require('./openaiconfig')
+const cors = require('cors');
+app.use(cors());
+app.use(express.json())
 
 
 
 
 const PORT=process.env.port||8080
-console.log(genImg(),PORT)
+//console.log(genImg('future technology'),PORT)
 
+
+app.get('/', async (req, res)=>{
+    res.json("YOOOOOOOOOOOOOOOOOOOO:!")
+})
 app.post('/genAiImage', async (req,res)=>{
-    console.log(req.body.query)
+    
+     console.log(req.body.query)
+     
+    try {
+     let img=await genImg(req.body.query)
+     console.log("THE iMAGE is:" + img)
+     res.send(img)
+    }
+    catch(err){
+        res.json(err)
+    }
 
 })
 
