d4b73ebb02504630e0e552163c0aa858997d37b9
mwezn <m.w.g.nelson@gmail.com>
2023-08-28 16:33:54 +0100

    made changes to mage sizes

--- openaiconfig.js
+++ openaiconfig.js
@@ -7,42 +7,22 @@ const configuration = new openai.Configuration({
 });
 const openai2 = new openai.OpenAIApi(configuration);
 async function genImg(queryTxt){
-  let image_url;
+  let image_urls;
     await openai2.createImage({
         prompt: `${queryTxt}`,
-        n: 1,
-        size: "1024x1024",
+        n: 5,
+        size: "512x512",
       })
       .then(d=>{
-          //console.log(d);
-          image_url=d.data.data[0].url;
-          console.log(image_url)
-          return image_url;
+          console.log(d);
+          image_urls=d.data.data;
+          console.log(image_urls)
+          return image_urls;
         })
       .catch(err=>console.log(err))
 
-      return image_url
+      return image_urls
 }
 
-async function txt2Cmd(){
-  await openai2.createCompletion({
-    model: "text-davinci-003",
-    prompt: "What goes up must",
-    temperature: 0,
-    max_tokens: 100,
-    top_p: 1.0,
-    frequency_penalty: 0.2,
-    presence_penalty: 0.0,
-    stop: ["\n"],
-  })
-  .then(d=>{
-    console.log(d.data);
-    return d;
-  })
-  .catch(err=>console.log(err))
 
-}
-
-txt2Cmd();
-
-module.exports= {genImg, txt2Cmd};
+module.exports= {genImg};
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express=require('express');
 const app=express();
-const {genImg, txt2Cmd}=require('./openaiconfig')
+const {genImg }=require('./openaiconfig')
 const cors = require('cors');
 app.use(cors());
 app.use(express.json())
@@ -12,27 +12,17 @@ const PORT=process.env.port||8080
 
 
 app.get('/', async (req, res)=>{
-    let resp=await txt2Cmd();
-    console.log(resp)
     res.sendFile(__dirname+'/index.html')
 })
 
-app.post('/getCompletion', async (req,res)=>{
-    
-    try {
-     let img=await txt2Cmd(req.body.query)
-     res.send({src:`${img}`})
-    }
-    catch(err){
-        res.json(err)
-    }
-})
+
 
 app.post('/genAiImage', async (req,res)=>{
     
     try {
      let img=await genImg(req.body.query)
-     res.send({src:`${img}`})
+     console.log(img)
+     res.send(img)
     }
     catch(err){
         res.json(err)
